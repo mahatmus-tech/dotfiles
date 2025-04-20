f637d3a283f331f6dc6a0f29a19fda0f736be37d
mahatmus <131696843+mahatmus-tech@users.noreply.github.com>
2025-04-19 23:27:24 -0300

    scripts

---
+++ scripts/camera-sara.sh
@@ -0,0 +1,1 @@
+ffplay rtsp://aranpasta:P9FMPYFkjElmr9SSa8tI@192.168.1.8/stream2 &
---
+++ scripts/remote-senior.sh
@@ -0,0 +1,2 @@
+# Open Senior Desktop
+rdesktop -g 1908x1025 -M -E -z -x l -d SENIOR -u ara.pasta -p Mahatma2002 192.168.1.22
