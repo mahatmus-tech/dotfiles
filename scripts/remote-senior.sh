1a0560429089e3ece65031ddc929a03a3e071194
mahatmus <131696843+mahatmus-tech@users.noreply.github.com>
2025-06-02 12:10:59 -0300

    senior ip update

--- scripts/remote-senior.sh
+++ scripts/remote-senior.sh
@@ -1,2 +1,2 @@
 # Open Senior Desktop
-rdesktop -g 1908x1025 -M -E -z -x l -d SENIOR -u ara.pasta -p Mahatma2003 192.168.1.22
+rdesktop -g 1908x1025 -M -E -z -x l -d SENIOR -u ara.pasta -p Mahatma2003 192.168.1.10
