533ba9a54b423adbde10cdb3f2e4a170719a1ce1
mahatmus <131696843+mahatmus-tech@users.noreply.github.com>
2025-06-19 16:02:06 -0300

    Update blstrobe-start.sh

--- scripts/blstrobe-start.sh
+++ scripts/blstrobe-start.sh
@@ -3,4 +3,5 @@
 # set this script in a local folder
 # set exec-once = ~/Documents/scripts/blstrobe-start.sh in hyprland.conf
 sudo modprobe i2c-dev
-sudo blstrobe -e -f -p 0 -o /dev/i2c-4 -t 5000 #brilho maximo
+#sudo blstrobe -e -f -p 0 -o /dev/i2c-4 -t 5000 #brilho maximo (30)
+sudo blstrobe -e -f -p 0 -o /dev/i2c-4 -t 3335 #brilho maximo (20)
